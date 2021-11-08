d4bbdedaf0eae230a530d6e5e8ca829682f42dd2
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2021-11-08 14:32:47 +0100

    Added widget object for input fields

---
+++ src/widgets/inputfield.ts
@@ -0,0 +1,21 @@
+import { Widget } from './widget';
+
+export class InputField extends Widget {
+
+	public async clear(): Promise<void> {
+		await (await this.elem).clearValue();
+	}
+
+	public async setText(text: string): Promise<void> {
+		await (await this.elem).setValue(text);
+	}
+
+	public async getText(): Promise<string> {
+		return (await this.elem).getAttribute('value');
+	}
+
+	public async isEnabled(): Promise<boolean> {
+		return (await this.elem).isEnabled();
+	}
+
+}
