ef3c42aa14dfd3820b091d116e7c7f4d0021518b
malemany <malemany@werfen.com>
2021-11-11 10:27:28 +0100

    wdio grid widget

---
+++ src/widgets/grid.ts
@@ -0,0 +1,89 @@
+import { Widget } from './widget';
+import { WebDriverIOElement, WebDriverIOElementArray } from '../types';
+import { ContextMenu } from './context-menu';
+
+export class Grid extends Widget {
+	public async getNumberOfRows(): Promise<number> {
+		return this.byCSS('.ag-center-cols-container').$$('div[role=row]').length
+	}
+
+	public async getValuesInRow(row: number): Promise<Array<string>> {
+		const content: string[] = [];
+		const cols:WebDriverIOElementArray = this.allByCSS(`div[row-index="${row}"] div.ag-cell-value`);
+		const numberOfRows: number = await cols.length;
+		for (let i = 0; i < numberOfRows; i++) {
+			const text: string = await (await cols)[i].getText();
+			content.push(text);
+		}
+		return content;
+	}
+
+	public async getValueInCell(row: number, columnName: string): Promise<string> {
+		const cellSelector = `[role='gridcell'][col-id='` + columnName + `']`;
+		const gridCell = (await $$(cellSelector))[row];
+		return gridCell.getText();
+	}
+
+	public async clickOnRowMenu(row: number): Promise<void> {
+		await this.clickOnLeftPinnedCell(row, 'contextMenu');
+	}
+
+	public async clickOnRow(row: number, column: string): Promise<void> {
+		await this.clickOnCell(row, column);
+	}
+
+	public async clickOnHeader(): Promise<void> {
+		// I click explicitly on the first element of the array, unlike in original.
+		await (await this.byClassName('ag-header-container').$('.ag-header-row').$$('ag-header-cell'))[0].click();
+	}
+
+	public async clickOnHeaderCell(columnIndex: number): Promise<void> {
+		await (await this.allByClassName('ag-header-cell-text'))[columnIndex - 1].click();
+	}
+
+	public async clickOnCell(row: number, column: string): Promise<void> {
+		// I click explicitly on the first element of the array, unlike in original.
+		await (await this.byClassName('ag-center-cols-container').$(`div[row-index="${row}"]`).$$(`div[col-id="${column}"`))[0].click();
+	}
+
+	public async clickOnLeftPinnedCell(row: number, column: string): Promise<void> {
+		// I click explicitly on the first element of the array, unlike in original.
+		await (await this.byClassName('ag-pinned-left-cols-container').$(`div[row-index="${row}"]`).$$(`div[col-id="${column}"]`))[0].click();
+	}
+
+	public async selectRow(row: number): Promise<void> {
+		// I click explicitly on the first element of the array, unlike in original.
+		await (await this.allByCSS(`div[row-index="${row}"] div[col-id="selectCol"] input`))[0].click();
+	}
+
+	public async getHeaderCaptions(): Promise<Array<string>> {
+		let content: string[] = [];
+		const cols: WebDriverIOElementArray = this.allByClassName('ag-header-cell');
+		const numberOfFields: number = await cols.length;
+		for (let i = 0; i < numberOfFields; i++) {
+			const text: string = await (await cols)[i].getText();
+			content.push(text);
+		}
+		return content;
+	}
+
+	public async getHeaderCells(): Promise<Array<WebDriverIOElement>> {
+		let content: WebDriverIOElement[] = [];
+		const cols: WebDriverIOElementArray = this.allByClassName('ag-header-cell');
+		const numberOfFields: number = await cols.length;
+		for (let i = 0; i < numberOfFields; i++) {
+			content.push((await cols)[i]);
+		}
+		return content;
+	}
+
+	public async getGridHeader(): Promise<Array<string>> {
+		// See if this applies https://github.com/angular/protractor/issues/3818
+		const cols: any = this.allByCSS('.ag-header-row .ag-header-cell-label');
+		return cols.getText();
+	}
+
+	public getMenu(): ContextMenu {
+		return new ContextMenu(this.byTagName('systelab-grid-content-menu'));
+	}
+}
