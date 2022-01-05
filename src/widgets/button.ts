22ec65fe1745de5f7670a2d4b4e9426465cccec0
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2022-01-05 14:13:46 +0100

    Added method to check if a button is clickable

--- src/widgets/button.ts
+++ src/widgets/button.ts
@@ -5,4 +5,8 @@ export class Button extends Widget {
 	public async click(): Promise<void> {
 		await (await this.elem).click();
 	}
+
+	public async isClickable(): Promise<boolean> {
+		return (await this.elem).isClickable();
+	}
 }
