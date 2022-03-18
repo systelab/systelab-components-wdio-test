d7735b6d72fc001782f4d6e738b92d1487c9ba41
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2022-03-18 16:49:56 +0100

    Updated combobox API with new methods that allow auto-closing it

--- src/widgets/combobox.ts
+++ src/widgets/combobox.ts
@@ -3,12 +3,34 @@ import { ElementArrayFinder } from "../wdio";
 
 
 export class ComboBox extends Widget {
-
     public async click(): Promise<void> {
-        return this.elem.click();
+        return this.elem.byCSS('dropdown-toggle').click();
+    }
+
+    public async isOptionsListOpen(): Promise<boolean> {
+        const nOptions = await this.allByCSS('.ag-cell-value').count();
+        return nOptions > 0;
+    }
+
+    public async openOptionsList(): Promise<void> {
+        const isOpen = await this.isOptionsListOpen();
+        if (!isOpen) {
+            await this.elem.click();
+            await this.waitUntil(async () => this.isOptionsListOpen());
+        }
+    }
+
+    public async closeOptionsList(): Promise<void> {
+        const isOpen = await this.isOptionsListOpen();
+        if (isOpen) {
+            await this.elem.click();
+            await this.waitUntil(async () => !(await this.isOptionsListOpen()));
+        }
     }
 
     public async getOptions(): Promise<string[]> {
+        await this.openOptionsList();
+
         let content: string[] = [];
         let rows: ElementArrayFinder = this.allByCSS('.ag-cell-value');
         let numberOfItems: number = await rows.count();
@@ -16,16 +38,20 @@ export class ComboBox extends Widget {
             let text: string = await rows.get(i).getText();
             content.push(text);
         }
-        return content;
-    }
 
-    public async selectOptionByNumber(i: number): Promise<void> {
-        return this.allByCSS(`[role='row'][row-index='` + i + `']`).get(1).click();
+        await this.closeOptionsList();
+
+        return content;
     }
 
     public async selectOptionByText(text: string): Promise<void> {
         const options = await this.getOptions();
         const optionIndex = options.findIndex((option) => option === text);
         return this.selectOptionByNumber(optionIndex);
+    }
+
+    public async selectOptionByNumber(i: number): Promise<void> {
+        await this.openOptionsList();
+        await this.allByCSS(`[role='row'][row-index='` + i + `']`).get(1).click();
     }
 }
