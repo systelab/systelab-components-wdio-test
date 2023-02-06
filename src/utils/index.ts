886ddc522ef462f8fecdd98d1e4767db2add7d45
jperez1-via <92033401+jperez1-via@users.noreply.github.com>
2023-02-06 10:58:30 +0100

    Clean up interface of screenshot utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './assertion.util';
 export * from './report.util';
 export * from './test-identification.util';
+export * from './screenshot.util';
--- src/utils/screenshot.util.ts
+++ src/utils/screenshot.util.ts
@@ -1,20 +1,32 @@
+import {readPngFileSync, writePngFileSync} from 'node-libpng';
+import {diffImages, DiffResult} from 'native-image-diff';
+
 import {ElementFinder} from "../wdio";
 import {ReportUtility} from "./report.util";
-import {readPngFileSync, writePngFileSync} from 'node-libpng';
-import {diffImages} from 'native-image-diff';
 import {AssertionUtility} from "./assertion.util";
 
-export class ScreenshotUtility {
 
-    public static async expectScreenshot(element: ElementFinder, screenshotPath: string, fileName: string, elementDescriptiveName: string, pixelTolerance: number = 0): Promise<void> {
-        const actualImageFilepath = `${screenshotPath}${fileName}-actual.png`;
+export class ScreenshotUtility {
+    private static basePath = './screenshots';
+    private static pixelTolerance = 0;
+
+    public static setBasePath(basePath: string): void {
+        this.basePath = basePath;
+    }
+
+    public static setPixelTolerance(pixelTolerance: number): void {
+        this.pixelTolerance = pixelTolerance;
+    }
+
+    public static async expectScreenshot(element: ElementFinder, fileName: string, elementDescriptiveName: string): Promise<void> {
+        const actualImageFilepath = `${this.basePath}/${fileName}-actual.png`;
         await element.saveScreenshot(actualImageFilepath);
 
-        const expectedImageFilepath = `${screenshotPath}${fileName}-expected.png`;
+        const expectedImageFilepath = `${this.basePath}/${fileName}-expected.png`;
         await ReportUtility.addExpectedResult(`'${elementDescriptiveName}' is equal to screenshot of '${fileName}-expected.png' file`, async () => {
-            const diffImageFilepath = `${screenshotPath}${fileName}-diff.png`;
+            const diffImageFilepath = `${this.basePath}/${fileName}-diff.png`;
             const diffPixels = this.compareImageFiles(expectedImageFilepath, actualImageFilepath, diffImageFilepath);
-            AssertionUtility.expectLowerOrEqual(diffPixels, pixelTolerance);
+            AssertionUtility.expectLowerOrEqual(diffPixels, this.pixelTolerance);
         });
     }
 
@@ -22,10 +34,11 @@ export class ScreenshotUtility {
         const expectedImage = readPngFileSync(expectedImageFilepath);
         const actualImage = readPngFileSync(actualImageFilepath);
 
-        const { image, pixels } = diffImages(expectedImage, actualImage);
-        // @ts-ignore
-        writePngFileSync(diffImageFilepath, image.data, { width: image.width, height: image.height });
+        const result: DiffResult = diffImages(expectedImage, actualImage);
+        if (result.image) {
+            writePngFileSync(diffImageFilepath, result.image.data, { width: result.image.width, height: result.image.height });
+        }
 
-        return pixels;
+        return result.pixels;
     }
 }
