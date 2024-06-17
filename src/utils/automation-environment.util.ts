9a298e4460d8883cd78ef64055317aac01847cf3
Joaquim Vila Bou <jvila2@werfen.com>
2024-06-17 17:46:48 +0200

    Handled browser object to be used when working in standalone mode

--- src/utils/automation-environment.util.ts
+++ src/utils/automation-environment.util.ts
@@ -15,18 +15,25 @@ export enum BrowserType {
 
 export class AutomationEnvironment {
     private static mode: AutomationMode = AutomationMode.Runner;
+    private static browser: any = null;
     private static browserType: BrowserType = BrowserType.Chrome;
 
     public static getMode(): AutomationMode {
         return this.mode;
     }
 
-    public static setTestRunnerMode(): void {
-        this.mode = AutomationMode.Runner;
+    public static getBrowser(): any {
+        return this.browser;
     }
 
-    public static setStandaloneMode(): void {
+    public static setTestRunnerMode(): void {
+        this.mode = AutomationMode.Runner;
+        this.browser = null; // Browser instance will be managed by test runner
+    }
+
+    public static setStandaloneMode(browser: any): void {
         this.mode = AutomationMode.Standalone;
+        this.browser = browser;
     }
 
     public static getBrowserType(): BrowserType {
