b92ae872055b4d72470bc99476e963af085ed305
Joaquim Vila Bou <jvila2@werfen.com>
2024-09-19 14:07:18 +0200

    Added JSON schema validation and support for basic element requests

---
+++ src/remote/server/schema/json-schema-validator.ts
@@ -0,0 +1,45 @@
+import Ajv from 'ajv';
+import fs from 'fs';
+import path from 'path';
+
+import { BasicElementRequest } from '../request/basic-element.request';
+
+
+interface LoadedSchema {
+    filename: string;
+    validateFn: any;
+}
+
+export class JSONSchemaValidator {
+    private static ajv = new Ajv();
+    private static loadedSchemas: LoadedSchema[] = [];
+
+    public static validateBasicElementRequest(data: unknown): BasicElementRequest {
+        this.validateData(data, 'basic-element-request-schema.json');
+        return data as BasicElementRequest;
+    }
+
+    private static validateData(data: unknown, schemaFilename: string): void {
+        const schema: LoadedSchema = this.loadSchema(schemaFilename);
+        const validData: boolean = schema.validateFn(data);
+        if (!validData) {
+            throw new Error(`Data does not fulfill JSON schema '${schemaFilename}':` + schema.validateFn.errors);
+        }
+    }
+
+    private static loadSchema(filename: string): LoadedSchema {
+        const loadedSchema = this.loadedSchemas.find((schema) => schema.filename === filename);
+        if (loadedSchema) {
+            return loadedSchema;
+        }
+
+        const schemaPath = path.resolve(filename);
+        const schemaFile: string = fs.readFileSync(schemaPath, 'utf-8');
+        const schemaObject: any = JSON.parse(schemaFile);
+        const compiledSchema = this.ajv.compile(schemaObject);
+        const newLoadedSchema: LoadedSchema = { filename, validateFn: compiledSchema };
+        this.loadedSchemas.push(newLoadedSchema);
+
+        return newLoadedSchema;
+    }
+}
