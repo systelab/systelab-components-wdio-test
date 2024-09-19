9b95cba3c927d0d6eb68aafc8f9b9e25b2dffaad
Joaquim Vila Bou <jvila2@werfen.com>
2024-09-19 14:48:02 +0200

    Set given application as current

--- src/remote/server/action.api.ts
+++ src/remote/server/action.api.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { ElementFinder, ElementFinderBuilder } from '../../wdio';
+import { AutomationEnvironment, ElementFinder, ElementFinderBuilder } from '../../wdio';
 import { JSONSchemaValidator } from './schema/json-schema-validator';
 import { BasicElementRequest } from './request/basic-element.request';
 
@@ -7,12 +7,17 @@ import { BasicElementRequest } from './request/basic-element.request';
 export class ActionAPI {
     public static async click(req: Request, res: Response): Promise<any> {
         try {
+            const applicationId: number = +req.params.id;
             const requestBody: BasicElementRequest = JSONSchemaValidator.validateBasicElementRequest(req.body);
+
+            AutomationEnvironment.setApplication(applicationId);
             const element: ElementFinder = ElementFinderBuilder.build(requestBody.locators) as ElementFinder;
             await element.click();
+
             return res.status(HttpStatus.NO_CONTENT).send();
         } catch (err) {
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({"error": err}).send();
         }
     }
+
 }
